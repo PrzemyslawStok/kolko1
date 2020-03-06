44d2d3880cc5ee947f35096b12f44affa2251a91
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-06 14:59:17 +0100

    cd..

--- src/Funkcje1.kt
+++ src/Funkcje1.kt
@@ -1,10 +1,11 @@
-import java.util.*
 import kotlin.math.log
 import kotlin.math.log10
 
 fun main(){
     val tab1 = arrayOf(1.0,100.0,100000.0,10.0,1000.0,1000000.0,1.0,0.0001,0.1)
-    val kwadraty = tab1.map{ log10(it) }
+    val logarytmy10 = tab1.map{ log10(it) }
 
-    println(kwadraty)
+    val log4 = log(16.0,4.0)
+
+    println(logarytmy10)
 }
