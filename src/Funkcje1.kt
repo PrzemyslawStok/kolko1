21bea84c1fe85ca5617ab8ffb37ecf49a36dc3c0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-03-06 14:50:33 +0100

    dodano logartymy z tablicy

--- src/Funkcje1.kt
+++ src/Funkcje1.kt
@@ -1,3 +1,10 @@
-fun main(){
+import java.util.*
+import kotlin.math.log
+import kotlin.math.log10
 
+fun main(){
+    val tab1 = arrayOf(1.0,100.0,100000.0,10.0,1000.0,1000000.0,1.0,0.0001,0.1)
+    val kwadraty = tab1.map{ log10(it) }
+
+    println(kwadraty)
 }
